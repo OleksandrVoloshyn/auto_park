804d0682fc3284ca2ddc70db10becb631cdb2556
OleksandrVoloshyn <OleksandrVoloshyn>
2021-03-06 18:04:16 +0200

    -change connect media to urlpatterns

--- user/models.py
+++ user/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
-
 class UserModel(AbstractUser):
     class Meta:
         db_table = 'auth_user'
