f8cd595763fa1d48f9461f6fe9d736122366885f
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-06-23 13:53:00 +0300

    done all tasks from first less

--- configs/urls.py
+++ configs/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
-from users.views import UsersListCreateView
+from users.views import UsersListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('users', UsersListCreateView.as_view()),
+    path('users/<int:pk>', UserRetrieveUpdateDestroyView.as_view())
 ]
