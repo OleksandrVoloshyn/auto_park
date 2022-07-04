0abd61167b2803df0854d3e04ebe3b705079e3cf
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-07-04 12:44:34 +0300

    done home task

--- apps/auto_parks/views.py
+++ apps/auto_parks/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from apps.cars.serializers import CarCreateSerializer
 
@@ -9,6 +10,7 @@ from .serializers import AutoParkSerializer
 class AutoParkListCreateView(ListCreateAPIView):
     queryset = AutoParksModel.objects
     serializer_class = AutoParkSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class AutoParkAddCarView(CreateAPIView):
